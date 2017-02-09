268740e106281fceada40559951d78f43719f392
Nils Jørgen Mittet <njmittet@gmail.com>
2017-02-09 18:16:36 +0100

    Support XML formatting of generic lists in the Response object

--- src/main/java/no/njm/model/Person.java
+++ src/main/java/no/njm/model/Person.java
@@ -1,12 +1,15 @@
 package no.njm.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 @XmlRootElement
-@XmlType(propOrder={"id", "firstName", "lastName"})
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
 
+    @XmlAttribute
     private int id;
     private String firstName;
     private String lastName;
@@ -20,27 +23,15 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public long getId() {
+    public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 }
--- src/main/java/no/njm/rest/PersonController.java
+++ src/main/java/no/njm/rest/PersonController.java
@@ -2,12 +2,14 @@ package no.njm.rest;
 
 import no.njm.data.PersonRepository;
 import no.njm.model.Person;
+import org.jboss.resteasy.annotations.providers.jaxb.Wrapped;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
@@ -16,19 +18,25 @@ import java.util.Optional;
 public class PersonController {
 
     @Inject
-    private PersonRepository persons;
+    private PersonRepository repository;
 
     @GET
+    @Wrapped(element = "persons")
     @Produces({"application/xml", "application/json"})
-    public List<Person> listPersons() {
-        return persons.listPersons();
+    public Response getPersons() {
+        List<Person> persons = repository.listPersons();
+
+        // Eliminate type erasure when wrapping List<> in a a Response object
+        GenericEntity<List<Person>> entity = new GenericEntity<List<Person>>(persons) {
+        };
+        return Response.ok(entity).build();
     }
 
     @GET
     @Path("/{id}")
     @Produces({"application/xml", "application/json"})
     public Response getPerson(@PathParam("id") Long id) {
-        Optional<Person> person = persons.getPerson(id);
+        Optional<Person> person = repository.getPerson(id);
         if (person.isPresent()) {
             return Response.ok(person.get()).build();
         }
